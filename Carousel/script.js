26eb32192a44c4b523f25b19db2acd43413ac2d8
Error Life 003 <errorlife003@gmail.com>
2025-05-07 23:22:53 +0530

    Update script.js

--- Carousel/script.js
+++ Carousel/script.js
@@ -2,12 +2,12 @@ const slider = document.querySelector('#slider');
 const totalSlides = slider.children.length;
 let currentIndex = 0;
 
-document.getElementById('next').addEventListener('click', () => {
+document.querySelector('#next').addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % totalSlides;
   slider.style.transform = `translateX(-${currentIndex * 100}%)`;
 });
 
-document.getElementById('prev').addEventListener('click', () => {
+document.querySelector('#prev').addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
   slider.style.transform = `translateX(-${currentIndex * 100}%)`;
 });
